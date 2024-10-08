4ddc06c9c6ffc461b516a068d6bff8e8eb4b7bd3
JoeyM25 <63532048+JoeyM25@users.noreply.github.com>
2024-10-08 14:02:44 -0500

    Ant movemnt losely working and added camera drag

--- ant.gd
+++ ant.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
-@export var speed = 2
+@export var speed: int
+@export var direction: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	speed = randf_range(1, speed)
+	direction = randf_range(0, direction)
+	# pass
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,16 +16,34 @@ func _process(delta):
 	if speed > 0:
 		$AnimatedSprite2D.play("default")
 		position.x += speed
-		if $RayCast2D.is_colliding():
-			$RayCast2D.rotation = 0
+		if $leftRight.is_colliding():
+			$leftRight.target_position.x = -16
 			speed = -speed
 	elif speed < 0:
 		position.x += speed
 		$AnimatedSprite2D.play("default")
-		if $RayCast2D.is_colliding():
-			$RayCast2D.rotation = 180
+		if $leftRight.is_colliding():
+			$leftRight.target_position.x = 16
 			speed = -speed
+	if direction < 0:
+		$upDown.target_position.y = 16
+		$AnimatedSprite2D.play("default")
+		position.y += direction
+		if $upDown.is_colliding():
+			$upDown.target_position.y = -16
+			direction = -direction
+	elif direction > 0:
+		$upDown.target_position.y = -16
+		$AnimatedSprite2D.play("default")
+		position.y += direction
+		if $upDown.is_colliding():
+			$upDown.target_position.y = 16
+			direction = -direction
+		
+		
+			
 
 func _on_hurt_box_body_entered(body):
 	if body.is_in_group("player"):
-		queue_free()
+		print("hit player")
+	
